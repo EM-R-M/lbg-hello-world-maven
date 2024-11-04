5106aa52b2caeda62b17b0316a048f53ca8f2c6f
EM-R-M <emily.manton@aol.com>
2024-11-04 13:52:12 +0000

    Update App.java
    adding myself into the app

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Emily"));
         System.out.println(sayGoodbye());
     }
 
